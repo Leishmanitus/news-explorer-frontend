0713027582b37d2e5190a9a2fe1b386f17556a0e
Tyler Leishman <tyler.leishman0@gmail.com>
2025-02-28 05:51:31 -0600

    delete redundant files.

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,18 +8,49 @@ export const request = (url, options) => {
   return fetch(url, options).then(handleResponse);
 };
 
-const updateUser = ({name, avatar}, token) => {
+const getArticlesList = () => {
+  return request(`${url}/saved-news`, {
+    method: "GET",
+    headers: {"Content-Type": "applicaiton/json",},
+  });
+};
+
+const addArticle = ({ name, imageUrl }, token) => {
+  return request(`${url}/saved-news`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify({ name, imageUrl }),
+  });
+};
+
+const removeArticle = (_id, token) => {
+  return request(`${url}/saved-news/${_id}`, {
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  });
+};
+
+const updateUser = ({ name }, token) => {
   return request(`${url}/users/me`, {
     method: "PATCH",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     },
-    body: JSON.stringify({ name, avatar }),
+    body: JSON.stringify({ name }),
   });
 };
 
 const api = {
+  getArticlesList,
+  addArticle,
+  removeArticle,
   updateUser,
 };
 
